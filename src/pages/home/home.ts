b44b96c349afe550cf1cd884cc1b02271d6dbd83
Calum Mackervoy <c.mackervoy@gmail.com>
2017-10-07 03:17:44 +0100

    Map in place

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -19,10 +19,7 @@ export class HomePage {
     if(this.hasMap === false) {
       this.hasMap = true;
 
-      this.map = L.map('map', {
-        center: [51.505, -0.09],
-        zoom: 13
-      });
+      this.map = L.map('map').setView([51.505, -0.09], 13);
 
       L.tileLayer('http://{s}.tile.cloudmade.com/e7b61e61295a44a5b319ca0bd3150890/997/256/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery Â© <a href="http://cloudmade.com">CloudMade</a>',
