1d7c99333b79b7664c29f9403111e504dfb76cfd
Calum Mackervoy <c.mackervoy@gmail.com>
2017-10-07 01:15:18 +0100

    Added Action Sheet to home
    Provides options for editing pins on the map

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ActionSheetController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,33 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public actionSheetCtrl: ActionSheetController) {
 
   }
 
+  openActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Update Map',
+      buttons: [
+        {
+          text: 'Add Pin',
+          handler: () => {
+            console.log('Add Pin clicked');
+          }
+        },{
+          text: 'Update Pin',
+          handler: () => {
+            console.log('Update Pin clicked');
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
 }
